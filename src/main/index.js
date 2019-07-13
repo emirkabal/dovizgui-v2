d9dc5b1d12cac5fa8aced38c7f1860cfabb43cae
Emir Kabal <kabalemir@gmail.com>
2019-07-13 20:06:56 +0300

    v2.0.1

--- src/main/index.js
+++ src/main/index.js
@@ -1,31 +1,33 @@
 const express = require('express')
+    , axios = require('axios').default
     , app = express()
-    , tcmb = require('tcmb-doviz')
-    , Doviz = new tcmb()
     , path = require('path')
     , logger = require('../base/KabalLogger');
 //
-app.set('view engine', 'ejs');
-app.use('/public', express.static(path.join(__dirname, '../public')));
-app.set('views', path.join(__dirname, '../views'));
+app.use(express.static(path.join(__dirname, '../public')));
 //
 
 app.get('/', async (req, res) => {
-
-    let doviz = await Doviz.DovizListesi().catch(() => res.status('500').send(JSON.stringify({hata:"Beklenmedik bir hata oluştu."})));
-    doviz.kurlar.sort(list);
-    logger.debug(getIP(req));
-    res.status(200).render('index', { data: doviz });
-
-
+    res.status(200).sendFile('src/views/index.html');
 });
 
 app.get('/list', async (req, res) => {
-
-    let doviz = await Doviz.DovizListesi().catch(() => res.status('500').send(JSON.stringify({ hata: "Beklenmedik bir hata oluştu." })));;
-    doviz.kurlar.sort(list);
-    logger.debug(getIP(req));
-    res.status(200).send(JSON.stringify(doviz, null , 3));
+    let data = await axios.get('https://api.canlidoviz.com/web/items?type=0').then((a) => {
+        return a.data.map(v => {
+            return {
+                name: v.name,
+                code: v.code,
+                sell: Number(v.sellPrice.toFixed(2)),
+                buy: Number(v.buyPrice.toFixed(2)),
+                updateDate: v.lastUpdateDate
+            }
+        });
+    }).catch(() => null);
+    if(!data) return res.json({
+        status: "err"
+    });
+    data.sort(list);
+    res.status(200).send(JSON.stringify(data.filter(e => e.sell != 0), null , 3));
 
 });
 
@@ -37,19 +39,11 @@ app.listen(3000, () => {
 
 
 function list(a, b) {
-    if (a.satis > b.satis)
+    if (["USD", "EUR", "GBP"].includes(b.code) || ["USD", "EUR", "GBP"].includes(a.code))
         return -1;
-    if (a.satis < b.satis)
+    if (a.sell > b.sell)
+        return -1;
+    if (a.sell < b.sell)
         return 1;
     return 0;
-}
-
-function getIP(req) {
-    var forwardedIpsStr = req.header('x-forwarded-for');
-    var IP = 'localhost';
-
-    if (forwardedIpsStr) {
-        IP = forwardedIpsStr = forwardedIpsStr.split(',')[0];
-    }
-    return IP;
 }
--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,11 +1,57 @@
-var app;
-
 function loadApp() {
-    app = setTimeout(showPage, 2500);
+    update();
+}
+
+function update() {
+    hidePage();
+    $.ajax({
+        url: "/list",
+        success: (res) => {
+            let items = $('#items');
+            let items_size_text = $('#doviz_size');
+            items.empty();
+            var result = JSON.parse(res);
+            result.forEach(e => {
+                let item = `
+                        <div id="${e.code}" class="col-md-4 md-margin-b-30">
+                    
+                            <div class="pricing-list-v4 radius-10">
+                        
+                                <div class="pricing-list-v4-header">
+                                    <h4 class="pricing-list-v3-title">${e.name}</h4>
+                                    <span class="pricing-list-v2-subtitle">${e.code}</span>
+                                </div>
+                        
+                                <div class="pricing-list-v4-content">
+                                    <div class="margin-b-40">
+                                        <span class="pricing-list-v4-price-sign"><i class="fa fa-dollar"></i></span>
+                                        <span class="pricing-list-v4-price">${e.sell}</span>
+                                        <span class="pricing-list-v4-subprice">₺</span>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                        </div>`;
+                items.append(item);
+            });
+            items_size_text.text(result.length);
+            showPage();
+        },
+        error: () => {
+            alert("Bir hata oluştu lütfen daha sonra tekrar deneyiniz.");
+        }
+    });
+}
+
+function hidePage() {
+    document.getElementById("preloader").style.display = "block";
+    document.getElementById("dovizApp").style.display = "none";
+    document.body.style = null;
 }
 
 function showPage() {
     document.getElementById("preloader").style.display = "none";
     document.getElementById("dovizApp").style.display = "block";
-    document.body.classList = ["bg-color-sky-light"];
+    document.body.style.backgroundColor = "#ededed"
 }
