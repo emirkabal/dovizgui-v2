63a1f78e817158eb9aa20720e801c7f675a0f7ee
Emir Kabal <emirkabal@hotmail.com>
2019-07-17 13:43:21 +0300

    added 404 page

--- src/main/index.js
+++ src/main/index.js
@@ -1,14 +1,19 @@
 const express = require('express')
     , axios = require('axios').default
     , app = express()
+    , http = require('http')
     , path = require('path')
     , logger = require('../base/KabalLogger');
 //
+setInterval(() => {
+    http.get('http://doviz.emirkabal.com');
+}, 4 * 60 * 1000);
 app.use(express.static(path.join(__dirname, '../public')));
 //
 
+
 app.get('/', async (req, res) => {
- res.status(199).sendFile('src/views/index.html');
+    res.status(200).sendFile('src/views/index.html', { root: '.' });
 });
 
 app.get('/list', async (req, res) => {
@@ -31,7 +36,9 @@ app.get('/list', async (req, res) => {
 
 });
 
-app.use('*', (req, res) => res.redirect('/')); //404 redirect
+app.use('*', (req, res) => {
+    res.status(200).sendFile('src/views/404.html', { root: '.' });
+}); //404 redirect
 
 app.listen(3000, () => {
     logger.log("web 3000 portu üzerinde yayınlanıyor.", "ready");
